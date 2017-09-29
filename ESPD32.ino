364774a1210b0b7ca4310cad61add5ffe96e7f90
mattheys <matt@matt-heys.co.uk>
2017-09-29 17:55:22 +0100

    Broke it

--- ESPD32.ino
+++ ESPD32.ino
@@ -5,12 +5,16 @@
 #define uS_TO_S_FACTOR 1000000 
 #include "time.h"
 #include "ssid.h"
+#include <ArduinoJson.h>
 
 unsigned char image[1024];
 Paint paint(image, 0, 0);
 Epd epd;
 
 RTC_DATA_ATTR int bootCount = 0;
+
+const size_t bufferSize = JSON_ARRAY_SIZE(3) + 4*JSON_OBJECT_SIZE(6) + 1350;
+DynamicJsonBuffer jsonBuffer(bufferSize);
 
 
 void setup() {
@@ -28,14 +32,14 @@ void setup() {
     epd.DisplayFrame();
     epd.ClearFrameMemory(0xFF);   // bit set = white, bit reset = black
     epd.DisplayFrame();
+  }
 
-    unsigned long timeout = millis() + 5000;
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED && millis()< timeout ) { delay(100); }
-
-    if(WiFi.status() == WL_CONNECTED) {
-      configTime(0, 3600, "pool.ntp.org");
-    }
+  //unsigned long timeout = millis() + 10000;
+  WiFi.begin(ssid, password);
+  //while (WiFi.status() != WL_CONNECTED && millis()< timeout ) { delay(100); }
+  while (WiFi.status() != WL_CONNECTED) { delay(100); }
+  if(WiFi.status() == WL_CONNECTED) {
+    configTime(0, 3600, "pool.ntp.org");
   }
 
   if (epd.Init(lut_partial_update) != 0) {
@@ -43,12 +47,18 @@ void setup() {
       return;
   }
 
-  //paint.DrawStringAt(4, 4, String("Boot Count " + String(bootCount)).c_str(), &Font16, 0);
-  //epd.SetFrameMemory(paint, 0, 0);
-  
+  //getJson();
+
   paint.SetRotate(ROTATE_0);
   printTime();
   printTrain();
+  weather(myBitmap1,0,52);
+  weather(myBitmap4,0,128);
+  weather(myBitmap2,64,52);
+  weather(myBitmap25,64,128);
+  weather(myBitmap20,128,52);
+  weather(myBitmap11,128,128);
+  
   epd.DisplayFrame();
 
 //  WiFiClient client;
@@ -125,11 +135,55 @@ void printTrain() {
   if(!getLocalTime(&timeinfo)){
         Serial.println("Failed to obtain time");
   }
-  char d[19] = "Town:15:51 On Time";
+  
+  //const char* ToTown = root["ToTown"];
+  
+  char d[19] = "Town:15:56 Early";
   char t[19] = "Stra:15:56 Early";
   
   paint.DrawStringAt(2,0,d,&Font16,0);
   paint.DrawStringAt(2,16,t,&Font16,0);
 
   epd.SetFrameMemory(paint, 0, 18);
+}
+
+void weather(const unsigned char* pic, int x, int y) {
+  epd.SetFrameMemory(pic,x,y,64,64);
+  paint.SetWidth(66);
+  paint.SetHeight(16);
+  paint.Clear(1);
+  paint.DrawStringAt(0,0,"16:00",&Font16,0);
+  epd.SetFrameMemory(paint,x+4,y+60);
+}
+
+void getJson() {
+  
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect("heys.suroot.com", httpPort)) {
+      Serial.println("connection failed");
+      return;
+  }
+
+    // We now create a URI for the request
+    String url = "/red/epd";
+
+    // This will send the request to the server
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: heys.suroot.com\r\n" +
+                 "Connection: close\r\n\r\n");
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 5000) {
+            Serial.println(">>> Client Timeout !");
+            client.stop();
+            return;
+        }
+    }
+
+    // Read all the lines of the reply from server and print them to Serial
+    String header = client.readStringUntil('\r\n\r\n');
+    const char* body = client.readString().c_str();
+    
+    JsonObject& poot = jsonBuffer.parseObject(body);
 }
--- icons.h
+++ icons.h
@@ -1,1 +1,6 @@
 extern const unsigned char myBitmap1[];
+extern const unsigned char myBitmap2[];
+extern const unsigned char myBitmap4[];
+extern const unsigned char myBitmap11[];
+extern const unsigned char myBitmap20[];
+extern const unsigned char myBitmap25[];
