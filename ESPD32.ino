2d89220cbd252ec9adffd626121df7b71a1b63b8
mattheys <matt@matt-heys.co.uk>
2017-09-29 16:11:33 +0100

    Updates

--- ESPD32.ino
+++ ESPD32.ino
@@ -1,100 +1,135 @@
 #include <SPI.h>
 #include <epd1in54.h>
-#include <epdpaint.h>
-#include "imagedata.h"
-#include "ssid.h"
 #include <WiFi.h>
+#include "icons.h"
+#define uS_TO_S_FACTOR 1000000 
+#include "time.h"
+#include "ssid.h"
 
- 
- 
 unsigned char image[1024];
-Paint paint(image, 0, 0);    // width should be the multiple of 8
+Paint paint(image, 0, 0);
 Epd epd;
-unsigned long time_start_ms;
-unsigned long time_now_s;
- 
+
+RTC_DATA_ATTR int bootCount = 0;
+
+
 void setup() {
-  // put your setup code here, to run once:
+
+  ++bootCount;
+  
   Serial.begin(115200);
+
+  if(bootCount == 1) {
+    if (epd.Init(lut_full_update) != 0) {
+      Serial.print("e-Paper init failed");
+      return;
+    }
+    epd.ClearFrameMemory(0xFF);   // bit set = white, bit reset = black
+    epd.DisplayFrame();
+    epd.ClearFrameMemory(0xFF);   // bit set = white, bit reset = black
+    epd.DisplayFrame();
+
+    unsigned long timeout = millis() + 5000;
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED && millis()< timeout ) { delay(100); }
+
+    if(WiFi.status() == WL_CONNECTED) {
+      configTime(0, 3600, "pool.ntp.org");
+    }
+  }
+
   if (epd.Init(lut_partial_update) != 0) {
       Serial.print("e-Paper init failed");
       return;
   }
- 
-WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
-      Serial.print(".");
-      delay(500);
-      paint.DrawStringAt(30, 4, "Connecting", &Font16, 1);
-      epd.SetFrameMemory(paint.GetImage(), 0, 10, paint.GetWidth(), paint.GetHeight());
-      epd.DisplayFrame();
-  }
-  paint.DrawStringAt(30, 4, "Connected", &Font16, 1);
-  epd.SetFrameMemory(paint.GetImage(), 0, 10, paint.GetWidth(), paint.GetHeight());
+
+  //paint.DrawStringAt(4, 4, String("Boot Count " + String(bootCount)).c_str(), &Font16, 0);
+  //epd.SetFrameMemory(paint, 0, 0);
+  
+  paint.SetRotate(ROTATE_0);
+  printTime();
+  printTrain();
   epd.DisplayFrame();
- 
-  epd.SetFrameMemory(myBitmap,8,8,96,96);
-  epd.DisplayFrame();
- 
-  WiFiClient client;
-    const int httpPort = 80;
-    if (!client.connect("matt-heys.co.uk", httpPort)) {
-        Serial.println("connection failed");
-        return;
-    }
- 
-    // This will send the request to the server
-    client.print(String("GET /testpic.php HTTP/1.1\r\n") +
-                 "Host: matt-heys.co.uk\r\n" +
-                 "Connection: close\r\n\r\n");
-    unsigned long timeout = millis();
-    while (client.available() == 0) {
-        if (millis() - timeout > 5000) {
-            Serial.println(">>> Client Timeout !");
-            client.stop();
-            return;
-        }
-    }
- 
-    // Read all the lines of the reply from server and print them to Serial
-    while(client.available()) {
-        String line = client.readStringUntil('\r');
-        if(line == "\r\n" || line == "" || line == "\r" || line == "\n")
-        {
-          unsigned char arr[5000];
-          client.readBytes(arr,5000);
-          epd.SetFrameMemory(arr);
-          epd.DisplayFrame();
-          return;
-        }
-    }
- 
-}
- 
-void loop() {
- 
- 
- 
- 
- 
-  //Serial.println("Loop");
-  //Serial.println("Loop");
-  // put your main code here, to run repeatedly:
-  //time_now_s = (millis() - time_start_ms) / 1000;
-  //char time_string[] = {'0', '0', ':', '0', '0', '\0'};
-  //time_string[0] = time_now_s / 60 / 10 + '0';
-  //time_string[1] = time_now_s / 60 % 10 + '0';
-  //time_string[3] = time_now_s % 60 / 10 + '0';
-  //time_string[4] = time_now_s % 60 % 10 + '0';
- 
-  //paint.SetWidth(32);
-  //paint.SetHeight(96);
-  //paint.SetRotate(ROTATE_270);
- 
-  //paint.Clear(UNCOLORED);
-  //paint.DrawStringAt(0, 4, time_string, &Font24, COLORED);
-  //epd.SetFrameMemory(paint.GetImage(), 80, 72, paint.GetWidth(), paint.GetHeight());
-  //epd.DisplayFrame();
+
+//  WiFiClient client;
+//  if (!client.connect("matt-heys.co.uk", 80)) {
+//        printError("connection failed");
+//        return;
+//  }
+//  
+//  client.print(String("GET /testpic.php HTTP/1.1\r\n") +
+//                 "Host: matt-heys.co.uk\r\n" +
+//                 "Connection: close\r\n\r\n");
+//  timeout = millis();
+//  while (client.available() == 0) {
+//    if (millis() - timeout > 5000) {
+//      printError(">>> Client Timeout !");
+//      client.stop();
+//      return;
+//    }
+//  }
+//
+//  while(client.available()) {
+//    String line = client.readStringUntil('\r');
+//    if(line == "\r\n" || line == "" || line == "\r" || line == "\n")
+//    {
+//      unsigned char arr[5000];
+//      client.readBytes(arr,5000);
+//      epd.SetFrameMemory(arr);
+//      epd.DisplayFrame();
+//      return;
+//    }
+//  }  
+
 }
 
+void loop() {
+  goToSleep(15);
+}
+
+void goToSleep(int seconds) {
+  epd.Sleep();
+  esp_sleep_enable_timer_wakeup(seconds * uS_TO_S_FACTOR);
+  esp_deep_sleep_start();
+}
+
+void printError(String error) {
+  Serial.println(error);
+}
+
+void printTime() {
+  paint.SetWidth(200);
+  paint.SetHeight(16);
+  paint.Clear(1);
+  
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+        Serial.println("Failed to obtain time");
+  }
+  char d[20];
+  
+  strftime(d, sizeof(d), "%b %d %H:%M", &timeinfo);
+  
+  
+  paint.DrawStringAt(34,0,d,&Font16,0);
+  epd.SetFrameMemory(paint, 0, 2);
+}
+
+void printTrain() {
+
+  paint.SetWidth(200);
+  paint.SetHeight(32);
+  paint.Clear(1);
+  
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+        Serial.println("Failed to obtain time");
+  }
+  char d[19] = "Town:15:51 On Time";
+  char t[19] = "Stra:15:56 Early";
+  
+  paint.DrawStringAt(2,0,d,&Font16,0);
+  paint.DrawStringAt(2,16,t,&Font16,0);
+
+  epd.SetFrameMemory(paint, 0, 18);
+}
